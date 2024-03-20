d0f3fc10b118af3824288f51802d842ca02bf85f
Ryan Gomes Ferreira da Silva <rm87243@estudante.fieb.edu.br>
2024-03-20 10:22:59 -0300

    alteraçôes 20/03/24 10:22

--- FRONT TCC 2.0/script.js
+++ FRONT TCC 2.0/script.js
@@ -3,28 +3,43 @@ function validar() {
     var nome = document.getElementById("nomefc").value;
     var email = document.getElementById("emailfc").value;
     var telefone = document.getElementById("telfc").value;
+    var select = document.getElementById("select");
 
-    if (nome ==""){
+    if (nome == "") {
         window.alert("informe o nome");
         document.getElementById("nomefc").focus();
-        document.getElementById("nomefc").style.borderColor="#ff0000";
+        document.getElementById("nomefc").style.borderColor = "#ff0000";
         return false
     }
 
 
-    if (email ==""){
+    if (email == "") {
         window.alert("informe o email");
         document.getElementById("emailfc").focus();
-        document.getElementById("emailfc").style.borderColor="#ff0000";
+        document.getElementById("emailfc").style.borderColor = "#ff0000";
         return false
     }
-    
-    if ( telefone ==""){
+
+    if (telefone == "") {
         window.alert("informe o Telefone");
         document.getElementById("telfc").focus();
-        document.getElementById("telfc").style.borderColor="#ff0000";
+        document.getElementById("telfc").style.borderColor = "#ff0000";
         return false
 
     }
-
+    if (select.options[select.selectedIndex].value == "") {
+        alert("Selecione uma opção antes de prosseguir");
+        document.getElementById("select").focus();
+        document.getElementById("select").style.borderColor = "#ff0000";
+        return false
+    }
+    if (comentario == "") {
+        window.alert("informe o Telefone");
+        document.getElementById("comentario").focus();
+        document.getElementById("telfc").style.borderColor = "#ff0000";
+        return false
+    }
 }
+
+
+
