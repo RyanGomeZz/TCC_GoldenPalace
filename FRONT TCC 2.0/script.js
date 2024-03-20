d46c2519ffd5c8ee058606bcc8eb6cd5e60e0360
Ryan Gomes Ferreira da Silva <rm87243@estudante.fieb.edu.br>
2024-03-20 11:07:55 -0300

    edição no script.js

--- FRONT TCC 2.0/script.js
+++ FRONT TCC 2.0/script.js
@@ -1,10 +1,12 @@
 function validar() {
-
+   
     var nome = document.getElementById("nomefc").value;
     var email = document.getElementById("emailfc").value;
     var telefone = document.getElementById("telfc").value;
     var select = document.getElementById("select");
-
+    var comentario = document.getElementById("comentario").value;
+    telefone = telefone.replace(/\D/g, '');
+    
     if (nome == "") {
         window.alert("informe o nome");
         document.getElementById("nomefc").focus();
@@ -27,6 +29,38 @@ function validar() {
         return false
 
     }
+    
+     if (isNaN(telefone)) {
+        
+        window.alert("Digite somente numeros ")
+        document.getElementById("telfc").focus();
+        document.getElementById("telfc").style.borderColor = "#ff0000";
+        return false
+        
+    }
+    //verificar quantidade de numeros correta
+    if (telefone.length <= 10 ) 
+    {
+        window.alert("numero a menos")
+        return false;
+    }
+
+    if (telefone.length >= 12 ) 
+    {
+        window.alert("numero a mais")
+        return false;
+    }
+
+
+
+    
+
+    //Se tiver 11 caracteres, verificar se começa com 9 o celular
+    if (telefone.length == 11 && parseInt(telefone.substring(2, 3)) != 9 ) {
+        window.alert("o numero está errado, não comeca com 9")
+        return false;
+    }
+
     if (select.options[select.selectedIndex].value == "") {
         alert("Selecione uma opção antes de prosseguir");
         document.getElementById("select").focus();
@@ -34,11 +68,19 @@ function validar() {
         return false
     }
     if (comentario == "") {
-        window.alert("informe o Telefone");
+        window.alert("Escreva algo");
         document.getElementById("comentario").focus();
-        document.getElementById("telfc").style.borderColor = "#ff0000";
+        document.getElementById("comentario").style.borderColor = "#ff0000";
         return false
     }
+
+    else{
+        
+        return true
+    }
+
+
+
 }
 
 
